feb49baf56a8acfc4ad350d3cc0ddc85d57563a6
Linda Abundis <linda_nayeli104@hotmail.com>
2021-03-05 01:32:47 +0000

    feature: Return an array of messages from server. JS fetchs that data and adds a random massage to the page

--- portfolio/src/main/java/com/google/sps/servlets/MyServletClass.java
+++ portfolio/src/main/java/com/google/sps/servlets/MyServletClass.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.*;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/message")
@@ -12,7 +13,22 @@ public class MyServletClass extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello world, I'm Linda!");
+    String[] bucketListTrips = {"India", "Italy", "Thailand"};
+
+    
+
+    // Convert the server stats to JSON
+    String json = convertToJsonUsingGson(bucketListTrips);
+
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+    
+  }
+
+  private String convertToJsonUsingGson(String[] bucketListTrips) {
+    Gson gson = new Gson();
+    String json = gson.toJson(bucketListTrips);
+    return json;
   }
 }
